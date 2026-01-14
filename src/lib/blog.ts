64828b98c96ccbd51bdb256e2b5fea3e4e89248e
Kyle <kyle.hebert@gmail.com>
2026-01-13 18:27:20 -0600

    add a relative date function for use on the home page

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -29,3 +29,18 @@ export const splitRecentBlogPosts = async (count: number) => {
     older: posts.slice(count),
   };
 };
+
+export const getRelativeDate = (pubDate: Date): string => {
+  const MS_PER_DAY = 24 * 60 * 60 * 1000
+  const now = new Date()
+  const differenceInDays = Math.floor((now.getTime() - pubDate.getTime()) / MS_PER_DAY )
+
+  if (differenceInDays <= 0) return "today"
+  if (differenceInDays === 1) return "yesterday"
+  if (differenceInDays < 7) return "a few days ago"
+  if (differenceInDays < 14) return "last week"
+  if (differenceInDays <28 ) return "a few weeks ago"
+  if (differenceInDays < 60 ) return "last month"
+
+    return pubDate.toLocaleDateString("en-US", { month: "long", day: "numeric", year: "numeric" });
+}
